0dfa0ba57da8ba2ab1cdfd521aa379fa761c8c27
ernestmatenjwa <alexmatenjwa@gmail.com>
2022-02-08 08:23:57 +0200

    First commit to Alex

---
+++ Screens/messagesScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+export default function MessengerScreen({ navigation }) {
+  return (
+    <View>
+       <Text>This is Messenger screen</Text>
+    </View>
+  );
+}
+
