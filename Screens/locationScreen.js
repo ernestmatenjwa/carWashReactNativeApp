66864089b194e3f12c0f9cd3cdd0a684e82e66ef
ernestmatenjwa <alexmatenjwa@gmail.com>
2022-02-09 10:16:46 +0200

    pulled

--- Screens/locationScreen.js
+++ Screens/locationScreen.js
@@ -4,42 +4,58 @@ import { Text,
          View, 
          StyleSheet,
         Dimensions,
-        TextInput } from 'react-native';
+        TextInput, SafeAreaView, FlatList } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import axios from "axios";
 
 const { width, height } = Dimensions.get("screen");
 
 export default function LocationScreen({ navigation }) {
+  
+  const [people, setPeople] = React.useState([
+    { name: 'shaun', id: '1' },
+    { name: 'yoshi', id: '2' },
+    { name: 'mario', id: '3' },
+    { name: 'luigi', id: '4' },
+    { name: 'peach', id: '5' },
+    { name: 'toad', id: '6' },
+    { name: 'bowser', id: '7' },
+  ]);
+
   return (
-  <View>
+  <SafeAreaView>
       <LinearGradient
    colors={["#064451", "#064451"]}
    style={styles.container} 
   >
-    <View style={{height: 10}}></View>
+  {/*}  <View style={{height: 10}}></View>
   <Text style={{fontSize:20, fontWeight: "bold"}}>   Select nearest service provider</Text>
   <TextInput 
     style={styles.seachInput}
     placeholder='   Search car wash'
-  />
+  />*/}
  </LinearGradient>
- <View style={{height: height/2.5, backgroundColor: "red"}}>
+ <View style={{height: height/2.3, backgroundColor: "red"}}>
     <Text onPress={() =>  navigation.push("ConfirmScreen")}>Map area</Text>
  </View>
- <View style={{height: height/3, backgroundColor: "green"}}>
-    <Text>items</Text>
+ <View style={{height: height/4, }}>
+ <FlatList 
+        numColumns={1}
+        keyExtractor={(item) => item.id} 
+        data={people} 
+        renderItem={({ item }) => ( 
+          <Text style={styles.item}>{item.name}</Text>
+        )}
+  />
  </View>
-  </View>
-
- 
- 
+  </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     width: "100%",
-    height: height / 7,
+    height: height / 5,
     borderBottomRightRadius: 20,
     borderBottomLeftRadius: 20,
   },
@@ -52,5 +68,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#f8f8ff",
     marginTop: height / 40,
     alignSelf: 'center'
-  }
+  },
+  item: {
+    flex: 1,
+    marginHorizontal: 10,
+    marginTop: 3,
+    padding: 30,
+    backgroundColor: '#fff',
+    fontSize: 24,
+  },
 })
