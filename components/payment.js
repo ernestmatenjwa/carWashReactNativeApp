709484ef418cea41975210af5bb83e2d72254a9c
Kaybee-debug <87021754+Kaybee-debug@users.noreply.github.com>
2022-02-11 08:41:32 +0200

    payment and date selector

--- components/payment.js
+++ components/payment.js
@@ -1,10 +1,166 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,StyleSheet,SafeAreaView,TextInput,Pressable,Image } from 'react-native';
+import COLORS from '../consts/colors';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import RadioForm, {
+  RadioButton, 
+  RadioButtonInput, 
+  RadioButtonLabel
+} from 'react-native-simple-radio-button';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+import bankCard from './../assets/pictures/bankCard.jpg';
+
 
 export default function PaymentScreen({ navigation }) {
+
+  var PaymentMethod = [
+    {label: "Credit Card", Value: 0},
+    {label: "Cash", Value: 1},
+   
+  ];
   return (
-    <View>
-       <Text>This is Payment screen</Text>
-    </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.white}}>
+      <View style={styles.header}>
+      <Icon name='arrow-back' size={28} onPress={() => navigation.goBack()}  style={{color: COLORS.tial, marginTop: 9, marginLeft: 10}}/>
+       <View style={{marginLeft: 35}}>
+         <Text style={{fontSize: 30, fontWeight: "bold", color: COLORS.tial}}>Payment Method</Text>
+         <Text style={{fontSize: 14, marginTop: 25, fontWeight: "bold", color: COLORS.tial}}>Select Your Payment Method</Text>
+       </View>
+      </View>
+
+      <View>
+           <RadioForm
+            onPress={(Value) => {}}
+            radio_props={PaymentMethod}
+            style={{flexDirection:'row',marginLeft:15}}
+           />
+         </View>
+
+      <Image source={bankCard} style={styles.image}/>
+
+      <View>
+
+       <Text style={styles.text_footer}>Card Number</Text>
+        <View style={styles.action}>
+            
+                  <TextInput
+                  placeholder="1234 4587 789 1234"
+                  styles={styles.textInput}
+                  autoCapitalize="none"
+                  />
+                
+             </View>
+
+             <Text style={styles.text_footer}>Name Of Card</Text>
+        <View style={styles.action} >
+            
+                  <TextInput
+                  placeholder="Not Smith"
+                  styles={styles.textInput}
+                  autoCapitalize="none"
+                  />
+                
+             </View>
+             <View style={{flexDirection:'row' }}>
+               <View style={{alignItem:'flex-start'}}>
+                 <Text style={styles.text_footer}>Expiration Date</Text>
+                 
+                 <TextInput
+                  placeholder="11 22"
+                  autoCapitalize="none"
+                  style={{ borderBottomWidth: 1,
+                    borderBottomColor: '#f2f2f2',}}
+                  />
+               </View>
+
+
+               <View style={{paddingLeft:150}}>
+                 <Text style={styles.text_footer}>Security Code</Text>
+                 <TextInput
+                  placeholder="***********"
+                  secureTextEntry={true}
+                  autoCapitalize="none"
+                  style={{ borderBottomWidth: 1,
+                    borderBottomColor: '#f2f2f2',}}
+                  />
+               </View>
+             </View>
+
+          <View>
+            <View style={{flexDirection:'row',marginTop:10 }}>
+             <View>
+              <Text>Subtotal</Text>
+              <Text>R120.00</Text>
+              </View>
+            </View>
+
+
+            <View>
+        <View></View>
+      <Pressable style={styles.bookbtn}
+      onPress={() => {
+        navigation.navigate("ConfirmScreen");
+      }}>
+        <Text style={{color: COLORS.white, fontSize: 18, fontWeight: 'bold'}}>Pay</Text>
+      </Pressable>
+      </View> 
+          </View>
+
+
+             </View>
+      </SafeAreaView>
+
   );
 }
+const styles = StyleSheet.create({
+  header:{
+    marginTop: 30,
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+  
+},
+textInput: {
+  flex: 1,
+  marginTop: Platform.OS === 'ios' ? 0 : -12,
+  paddingLeft: 10,
+  color: '#05375a',
+  
+},
+text_footer: {
+  color: '#05375a',
+  fontSize: 15,
+  marginTop:25
+},
+bookbtn:{
+  width: 120,
+  height: 50,
+  backgroundColor: COLORS.tial,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 10,
+  marginRight:10,
+  alignSelf:'flex-end',
+  
+
+ 
+},
+image:{
+marginLeft:10
+
+
+  
+
+}
+  
+
+});
+
+
